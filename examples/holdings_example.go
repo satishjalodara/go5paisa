ea752b44d57b1227238c30425f79bc1d0d127dde
satishjalodara <75116139+satishjalodara@users.noreply.github.com>
2021-11-24 15:58:22 +0530

    Update holdings_example.go

--- examples/holdings_example.go
+++ examples/holdings_example.go
@@ -9,15 +9,15 @@ import (
 func main() {
 
 	conf := &go5paisa.Config{
-		AppName:       "YOUR_APP_NAME_HERE",
-		AppSource:     "YOUR_APP_SOURCE_HERE",
-		UserID:        "YOUR_USER_ID_HERE",
-		Password:      "YOUR_PASSWORD_HERE",
-		UserKey:       "YOUR_USER_KEY_HERE",
-		EncryptionKey: "YOUR_ENCRYPTION_KEY_HERE",
+		AppName:       "5P53076904",
+		AppSource:     "4223",
+		UserID:        "5pcntfcOscA",
+		Password:      "qv5GtT63l80",
+		UserKey:       "UnSC1c4nrRhMmJMahoZmlN8jXo6Rvruu",
+		EncryptionKey: "wPTRvBs6xLrL5cJFmwibXSWhFHaGTdSc",
 	}
 	appConfig := go5paisa.Init(conf)
-	client, err := go5paisa.Login(appConfig, "xyz@gmail.com", "password", "YYYYMMDD")
+	client, err := go5paisa.Login(appConfig, "8vQRCYZWhIrQ8RREx7GKHolrWe6jqlgZb3zjvxVHpJI=", "rPLQ/ENmoy4GVyMXt8x3FA==", "8hgQM+jqMESAV1nxOoN")
 	if err != nil {
 		panic(err)
 	}
